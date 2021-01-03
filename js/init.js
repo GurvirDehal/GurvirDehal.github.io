e40ff48d5bd6b484afad6e8bf13e69d381623652
Gurvir Dehal <gd1544@students.cs.ubc.ca>
2021-01-03 01:52:06 -0700

    Updates

--- js/init.js
+++ js/init.js
@@ -3,9 +3,56 @@
 /* Init JS
 /*
 -----------------------------------------------------------------------------------*/
+var TxtRotate = function(el, toRotate, period) {
+   this.toRotate = toRotate;
+   this.el = el;
+   this.loopNum = 0;
+   this.period = parseInt(period, 10) || 2000;
+   this.txt = '';
+   this.tick();
+   this.isDeleting = false;
+ };
+ 
+ TxtRotate.prototype.tick = function() {
+   var i = this.loopNum % this.toRotate.length;
+   var fullTxt = this.toRotate[i];
+ 
+   if (this.isDeleting) {
+     this.txt = fullTxt.substring(0, this.txt.length - 1);
+   } else {
+     this.txt = fullTxt.substring(0, this.txt.length + 1);
+   }
+ 
+   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+ 
+   var that = this;
+   var delta = 100;
+ 
+   if (this.isDeleting) { delta /= 2; }
+ 
+   if (!this.isDeleting && this.txt === fullTxt) {
+     delta = this.period;
+     this.isDeleting = true;
+   } else if (this.isDeleting && this.txt === '') {
+     this.isDeleting = false;
+     this.loopNum++;
+     delta = 500;
+   }
+ 
+   setTimeout(function() {
+     that.tick();
+   }, delta);
+ };
 
  jQuery(document).ready(function($) {
-
+   const elements = document.getElementsByClassName('txt-rotate');
+   for (let i=0; i<elements.length; i++) {
+     const toRotate = elements[i].getAttribute('data-rotate');
+     const period = elements[i].getAttribute('data-period');
+     if (toRotate) {
+       new TxtRotate(elements[i], JSON.parse(toRotate), period);
+     }
+   }
 /*----------------------------------------------------*/
 /* FitText Settings
 ------------------------------------------------------ */
